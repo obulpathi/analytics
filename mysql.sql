5651c9306aeaa231f2a8bd0aa54fd3336cda18e4
Obulpathi <obulapathi.challa@rackspace.com>
2015-02-02 14:21:27 -0500

    Adds mysql.sql

---
+++ mysql.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE poppy;
+USE poppy;
+
+CREATE TABLE countries (userid INT, USA INT, India INT, China INT, Russia INT, Germany INT);
+INSERT INTO countries VALUES (1, 30, 20, 20, 5, 5);
+
+CREATE TABLE browsers (userid INT, Chrome INT, Firefox INT, IE INT, Opera INT);
+INSERT INTO browsers VALUES (1, 35, 25, 10, 10);
+
+CREATE TABLE days (userid INT, day1 INT, day2 INT, day3 INT, day4 INT);
+INSERT INTO days VALUES (1, 25, 10, 35, 40);
